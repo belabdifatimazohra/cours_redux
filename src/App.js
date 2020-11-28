114cd1dfd53aaad19313f6d64cd79181a560c38a
Fatima Zohra <fatima.zohra.belabdi@gmail.com>
2020-11-28 22:04:19 +0100

    Dispatch & selector

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-import { React, useState, useDispatch } from "react";
+import { React, useState } from "react";
 import Todo from "./components/Todo"
 // Import function actions
 import {addTodo,deletList,editList} from './redux/Actions/ActionTodoList'
 import { v4 as uuidv4 } from 'uuid';
+import {useDispatch,useSelector} from 'react-redux'
 
 
 function App() {
   // input State
   const [input, setInput] = useState();
+  // dispatch info state
   const dispatch = useDispatch();
+  const Todos = useSelector((state)=>state.todos);
+  console.log(Todos);
   return (
     <div >
       <div className="header">
@@ -19,6 +23,7 @@ function App() {
         dispatch(addTodo({id:uuidv4(), task: input, check: false}))
         }>Add</span>
       </div>
+      
     </div>
   );
 }
